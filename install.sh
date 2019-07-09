e6685da57dd3ada48afb611d6b3b3d9a2748bbf9
filhit <filhit@gmail.com>
2019-07-09 18:14:45 +0200

    Add packages

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
+sudo add-apt-repository ppa:minetestdevs/stable
+sudo apt-get update
+sudo apt-get install minetest
 sudo cp minetest-server.service /etc/systemd/system/
 sudo systemctl enable minetest-server
 sudo systemctl start minetest-server
