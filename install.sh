21a9de35002a84d5d9b44223569fa36077d6b092
filhit <filhit@gmail.com>
2019-07-09 18:26:19 +0200

    Add awscli package

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -e
 sudo add-apt-repository ppa:minetestdevs/stable
 sudo apt-get update
-sudo apt-get install minetest
+sudo apt-get install minetest awscli
 sudo cp minetest-server.service /etc/systemd/system/
 sudo systemctl enable minetest-server
 sudo systemctl start minetest-server
