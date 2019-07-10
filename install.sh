98caf826ea0573f25745959ec4e1671caf9da649
filhit <filhit@gmail.com>
2019-07-10 16:15:24 +0000

    Add aws configure

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -e
 sudo add-apt-repository ppa:minetestdevs/stable
 sudo apt-get update
 sudo apt-get install minetest awscli
+aws configure
 sudo cp minetest-server.service /etc/systemd/system/
 sudo systemctl enable minetest-server
 sudo systemctl start minetest-server
