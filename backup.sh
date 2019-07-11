c266dfd3b1aa7958a469ec4204904ccc1c3fbcce
filhit <filhit@gmail.com>
2019-07-11 09:09:27 +0000

    Give archive a proper extension

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 sudo systemctl stop minetest-server
-tar -cv /home/filhit/.minetest | gzip | aws s3 cp - s3://dsoulstest-server-backups/minetest.gz
+tar -cv /home/filhit/.minetest | gzip | aws s3 cp - s3://dsoulstest-server-backups/minetest.tar.gz
 sudo systemctl start minetest-server
