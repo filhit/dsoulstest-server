7ca3430f383f037bd8966db17ccc7319591f2a22
filhit <filhit@gmail.com>
2019-07-19 11:39:22 +0000

    Add option to skip remote backup

--- backup.sh
+++ backup.sh
@@ -5,8 +5,12 @@ echo "Stopping service"
 sudo systemctl stop minetest-server
 echo "Backing up to file"
 tar -cv /home/filhit/.minetest | gzip > /home/filhit/minetest-backups/minetest.tar.gz
-echo "Backing up to aws s3"
-aws s3 cp /home/filhit/minetest-backups/minetest.tar.gz s3://dsoulstest-server-backups/minetest.tar.gz
-/home/filhit/dsoulstest-server/update-maps.sh
+if [ "$1" == "--no-remote" ]; then
+  echo "Skipping remote backup"
+else
+  echo "Backing up to aws s3"
+  aws s3 cp /home/filhit/minetest-backups/minetest.tar.gz s3://dsoulstest-server-backups/minetest.tar.gz
+  /home/filhit/dsoulstest-server/update-maps.sh
+fi
 echo "Starting service"
 sudo systemctl start minetest-server
