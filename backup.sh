6f3f7b98c9ff0d6b9bcfff032df603196647d5a8
filhit <filhit@gmail.com>
2019-08-20 09:29:31 +0000

    Remove progress from logs

--- backup.sh
+++ backup.sh
@@ -4,12 +4,12 @@ echo "Rotating local backups"
 echo "Stopping minetest-server service"
 sudo systemctl stop minetest-server
 echo "Backing up to file"
-tar -C /home/filhit/ -cv .minetest | gzip > /home/filhit/minetest-backups/minetest.tar.gz
+tar -C /home/filhit/ -c .minetest | gzip > /home/filhit/minetest-backups/minetest.tar.gz
 if [ "$1" == "--no-remote" ]; then
   echo "Skipping remote backup"
 else
   echo "Backing up to aws s3"
-  aws s3 cp /home/filhit/minetest-backups/minetest.tar.gz s3://dsoulstest-server-backups/minetest.tar.gz
+  aws s3 cp /home/filhit/minetest-backups/minetest.tar.gz s3://dsoulstest-server-backups/minetest.tar.gz --no-progress
   /home/filhit/dsoulstest-server/update-maps.sh
 fi
 echo "Starting minetest-server service"
