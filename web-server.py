545a7978fd2cb70fa84644c5e728e1e416d5611e
Andrey Rjeutski <filhit@gmail.com>
2019-09-05 20:05:20 +0200

    Add web server

---
+++ web-server.py
@@ -0,0 +1,46 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from datetime import datetime
+import json
+import ssl
+
+last_update = None
+last_players = None
+
+class HTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def create_response(self):
+        response = {}
+
+        if last_update is None:
+            response['status'] = 'Error'
+        else:
+            response['last_update'] = last_update.isoformat()
+
+        if last_players is not None:
+            response['players'] = last_players
+
+        return response
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(json.dumps(self.create_response()).encode())
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        request = json.loads(body.decode())
+        global last_players
+        last_players = request['players']
+        self.send_response(200)
+        self.end_headers()
+        global last_update
+        last_update = datetime.now()
+
+httpd = HTTPServer(('localhost', 30001), HTTPRequestHandler)
+httpd.socket = ssl.wrap_socket (httpd.socket,
+        keyfile="/etc/letsencrypt/live/minetest.westeurope.cloudapp.azure.com/privkey.pem",
+        certfile='/etc/letsencrypt/live/minetest.westeurope.cloudapp.azure.com/fullchain.pem',
+        server_side=True)
+
+httpd.serve_forever()
